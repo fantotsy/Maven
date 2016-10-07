8ff8c4b9c9e23f7c72d19999d2a97bc9d8c9a902
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-10-07 13:05:18 +0300

    App class modified and exec plugin added.

--- src/main/java/ua/fantotsy/App.java
+++ src/main/java/ua/fantotsy/App.java
@@ -1,13 +1,11 @@
 package ua.fantotsy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        StringBuilder output = new StringBuilder();
+        for (String arg : args) {
+            output.append(arg);
+        }
+        System.out.println(output.toString());
     }
 }
