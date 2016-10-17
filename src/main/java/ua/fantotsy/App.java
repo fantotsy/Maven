9df8ba86f7c95e0208e9c56038c25113fbd94102
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-10-17 15:04:10 +0300

    Database & liquibase connected.

--- src/main/java/ua/fantotsy/App.java
+++ src/main/java/ua/fantotsy/App.java
@@ -1,5 +1,6 @@
 package ua.fantotsy;
 
+import java.sql.*;
 import java.util.Arrays;
 
 public class App {
@@ -10,6 +11,33 @@ public class App {
         } else {
             System.out.println("There are no arguments!");
         }
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        Connection connection = null;
+        String url = "jdbc:oracle:thin:@localhost:1521:xe";
+        String user = "fantotsy";
+        String password = "root";
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT st_name FROM students");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
     }
 
     public static boolean containsArguments(String[] args) {
